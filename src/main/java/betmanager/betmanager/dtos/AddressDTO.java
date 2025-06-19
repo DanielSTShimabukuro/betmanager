70cce4ce22cf4948d38251aa15c7f3a5701e997a
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-06-18 21:04:44 -0300

    refactor(AddressDTO): rename number to numberStreet

--- src/main/java/betmanager/betmanager/dtos/AddressDTO.java
+++
@@ -1,5 +0,0 @@
-package betmanager.betmanager.dtos;
-
-public record AddressDTO(String cep, String street, int number, String comp, String city, String state) {
-  
-}
---
+++ src/main/java/betmanager/betmanager/dtos/address/AddressDTO.java
@@ -0,0 +1,5 @@
+package betmanager.betmanager.dtos.address;
+
+public record AddressDTO(String ddd, String cep, String street, int numberStreet, String comp, String city, String state) {
+  
+}
