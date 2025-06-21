35665a7b2b17cd88eb0b1a71a8e95532db5594a4
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-06-21 18:08:42 -0300

    feat: add findAddressById function

---
+++ src/main/java/betmanager/betmanager/repositories/Adress/AddresRepository.java
@@ -0,0 +1,11 @@
+package betmanager.betmanager.repositories.Adress;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import betmanager.betmanager.models.adress.Address;
+
+public interface AddresRepository extends JpaRepository<Address, String> {
+  Optional<Address> findAddressById(String id);
+}
